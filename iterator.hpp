166ef4444573e1c47d7df54876e61129d7a7e9ff
Daniel Suchkov <suc-daniil@yandex.ru>
2015-10-09 03:15:54 +0600

    add another range function, rename old ones, fix tabulation

--- iterator.hpp
+++ iterator.hpp
@@ -7,22 +7,29 @@
 
 namespace fcl {
     template <typename... T>
-	auto zip(const T&... containers)
-	    -> boost::iterator_range<boost::zip_iterator<decltype(boost::make_tuple(std::begin(containers)...))>> {
-	    auto zip_begin = boost::make_zip_iterator(boost::make_tuple(std::begin(containers)...));
-	    auto zip_end = boost::make_zip_iterator(boost::make_tuple(std::end(containers)...));
-	    return boost::make_iterator_range(zip_begin, zip_end);
-	}
+    auto zip(const T&... containers)
+        -> boost::iterator_range<boost::zip_iterator<decltype(boost::make_tuple(std::begin(containers)...))>> {
+        auto zip_begin = boost::make_zip_iterator(boost::make_tuple(std::begin(containers)...));
+        auto zip_end = boost::make_zip_iterator(boost::make_tuple(std::end(containers)...));
+        return boost::make_iterator_range(zip_begin, zip_end);
+    }
 
-	template <typename N>
-	auto range(const N from, const N to)
-	    -> decltype(boost::make_iterator_range(boost::counting_iterator<N>(from), boost::counting_iterator<N>(to))) {
-	    return boost::make_iterator_range(boost::counting_iterator<N>(from), boost::counting_iterator<N>(to));
-	}
+    template <typename Iter>
+    auto range(Iter from, Iter to)
+        -> decltype(boost::make_iterator_range(from, to)) {
+        return boost::make_iterator_range(from, to);
+    }
 
-	template
-	auto rrange(const N from, const N downto)
-	    -> {
-	    return boost::adaptors::reverse(range(downto, from));
-	}
+
+    template <typename N>
+    auto nrange(const N from, const N to)
+        -> range(boost::counting_iterator<N>(from), boost::counting_iterator<N>(to))) {
+        return range(boost::counting_iterator<N>(from), boost::counting_iterator<N>(to));
+    }
+
+    template
+    auto rnrange(const N from, const N downto)
+        -> {
+        return boost::adaptors::reverse(nrange(downto, from));
+    }
 }
